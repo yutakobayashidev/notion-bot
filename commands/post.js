c8373223a05ebf6e81b32a19e25f79be388dd7a6
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-03-03 18:26:35 +0900

    add: query

--- commands/post.js
+++ commands/post.js
@@ -3,41 +3,139 @@ const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID;
 
+const now = new Date();
+const yesterday = new Date(
+  now.getFullYear(),
+  now.getMonth(),
+  now.getDate() - 1
+);
+
 module.exports = {
   data: {
     name: "post",
     description: "ページを作成します。",
     options: [
       {
-        type: "STRING",
-        name: "title",
-        description: "タイトルを入力します。",
-        required: true,
+        type: "SUB_COMMAND",
+        name: "pages",
+        description: "ページを作成します。",
+        options: [
+          {
+            type: "STRING",
+            name: "title",
+            description: "タイトルを入力します。",
+            required: true,
+          },
+        ],
+      },
+      {
+        type: "SUB_COMMAND",
+        name: "query",
+        description: "データベースをフィルターして検索します。",
+        options: [
+          {
+            type: "STRING",
+            name: "type",
+            description:
+              "期間切れタスクか未スケジューリングタスクなのかを選択します",
+            required: true,
+            choices: [
+              { name: "期間切れタスク", value: "expired" },
+              { name: "未スケジューリングタスク", value: "unschedule" },
+            ],
+          },
+        ],
       },
     ],
   },
   async execute(interaction) {
-    const response = await notion.pages.create({
-      parent: {
-        database_id: databaseId,
-      },
-      properties: {
-        タスク名: {
-          title: [
-            {
-              text: {
-                content: interaction.options.getString("title"),
-              },
+    if (interaction.commandName === "post") {
+      if (interaction.options.getSubcommand() === "pages") {
+        const title = interaction.options.getString("title");
+        const response = await notion.pages.create({
+          parent: {
+            database_id: databaseId,
+          },
+          properties: {
+            タスク名: {
+              title: [
+                {
+                  text: {
+                    content: title,
+                  },
+                },
+              ],
             },
-          ],
-        },
-      },
-    });
-    const url = response["url"];
-    await interaction.reply({
-      content:
-        `「${interaction.options.getString("title")}」を作成しました\n` + url,
-      ephemeral: true,
-    });
+          },
+        });
+        const url = response["url"];
+        await interaction.reply({
+          content: `「${title}」を作成しました\n` + url,
+          ephemeral: true,
+        });
+      } else if (interaction.options.getSubcommand() === "query") {
+        if (interaction.options.getString("type") === "expired") {
+          const response = await notion.databases.query({
+            database_id: databaseId,
+            filter: {
+              and: [
+                {
+                  property: "Status",
+                  select: {
+                    does_not_equal: "Done",
+                  },
+                },
+                {
+                  property: "Status",
+                  select: {
+                    does_not_equal: "Pending",
+                  },
+                },
+                {
+                  property: "Date",
+                  date: {
+                    before: yesterday,
+                  },
+                },
+              ],
+            },
+          });
+          console.log(response);
+          await interaction.reply({
+            content: `期間切れタスクが${
+              Object.keys(response["results"]).length
+            }件見つかりました。`,
+            ephemeral: true,
+          });
+        } else if (interaction.options.getString("type") === "unschedule") {
+          const response = await notion.databases.query({
+            database_id: databaseId,
+            filter: {
+              and: [
+                {
+                  property: "Status",
+                  select: {
+                    does_not_equal: "Done",
+                  },
+                },
+                {
+                  property: "Date",
+                  date: {
+                    is_empty: true,
+                  },
+                },
+              ],
+            },
+          });
+          console.log(response);
+          await interaction.reply({
+            content: `期間切れタスクが${
+              Object.keys(response["results"]).length
+            }件見つかりました。`,
+            ephemeral: true,
+          });
+        }
+      }
+    }
   },
 };
