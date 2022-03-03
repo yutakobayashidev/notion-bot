fb03015fe71eee20c87f454f045bec54bc98d4c9
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-03-03 16:40:10 +0900

    rename

--- commands/post.js
+++ commands/post.js
@@ -1,32 +1,31 @@
 const { Client } = require("@notionhq/client");
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
-const database_id = process.env.NOTION_DATABASE_ID;
+const databaseId = process.env.NOTION_DATABASE_ID;
 
 module.exports = {
   data: {
-    name: "inbox",
-    description: "INBOXにタスクを追加します",
+    name: "post",
+    description: "ページを作成します。",
     options: [
       {
         type: "STRING",
-        name: "タイトル",
+        name: "title",
         description: "タイトルを入力します。",
-        required: true,
       },
     ],
   },
   async execute(interaction) {
     const response = await notion.pages.create({
       parent: {
-        database_id: database_id,
+        database_id: databaseId,
       },
       properties: {
         タスク名: {
           title: [
             {
               text: {
-                content: interaction.options.getString("タイトル"),
+                content: interaction.options.getString("title"),
               },
             },
           ],
@@ -36,8 +35,7 @@ module.exports = {
     const url = response["url"];
     await interaction.reply({
       content:
-        `「${interaction.options.getString("タイトル")}」を作成しました\n` +
-        url,
+        `「${interaction.options.getString("title")}」を作成しました\n` + url,
       ephemeral: true,
     });
   },
